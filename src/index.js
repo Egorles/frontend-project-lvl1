ad178f70642c45fca048bf53a39d5ebed10c7b77
EgorLes <egor.lenivin@gmail.com>
2019-10-28 11:05:04 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export const greeting = () => {
-	console.log('Welcome to the Brain Games!');
-	
-	console.log(`Hello, ${readlineSync.question('May I have your name? ')}!`);
+  console.log('Welcome to the Brain Games!');
+  console.log(`Hello, ${readlineSync.question('May I have your name? ')}!`);
 };
